b14fc4e5cf7a95b6c672b0afbe543da873f05fd8
JuliyaEduardovna <juliya.eduardovna@gmail.com>
2025-11-28 02:21:10 +0700

    5.8. Модуляция

---
+++ js/util.js
@@ -0,0 +1,20 @@
+import { MESSAGE } from './data.js';
+
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (elements) =>
+  elements[getRandomInteger(0, elements.length - 1)];
+
+const getRandomMessage = () => {
+  const firstSentence = getRandomArrayElement(MESSAGE);
+  return getRandomInteger(1, 2) === 1
+    ? firstSentence
+    : `${firstSentence} ${getRandomArrayElement(MESSAGE)}`;
+};
+
+export { getRandomInteger, getRandomArrayElement, getRandomMessage };
