d97cbdb4b9060b76d029958ecbefd6337138a70f
JuliyaEduardovna <juliya.eduardovna@gmail.com>
2025-12-15 19:11:06 +0700

    9.13. Помощь друга

--- js/constants.js
+++ js/constants.js
@@ -42,4 +42,39 @@ const DESCRIPTION = [
   'Скальная арка над морем, где волны бьют строго по KPI приливов.',
 ];
 
-export {PICTURE_COUNT, LIKE_MIN, LIKE_MAX, AVATAR_COUNT, COMMENT_COUNT, NAME, MESSAGE, DESCRIPTION, COMMENTS_PER_PAGE, REGEX_FOR_HASHTAG, HASHTAG_COUNT_MAX, HASHTAG_LENGTH_MAX, DESCRIPTION_LENGTH_MAX};
+const EFFECTS = {
+  none: { filter: null },
+  chrome: {
+    filter: 'grayscale',
+    min: 0,
+    max: 1,
+    step: 0.1,
+    unit: '',
+    start: 1,
+  },
+  sepia: { filter: 'sepia', min: 0, max: 1, step: 0.1, unit: '', start: 1 },
+  marvin: {
+    filter: 'invert',
+    min: 0,
+    max: 100,
+    step: 1,
+    unit: '%',
+    start: 100,
+  },
+  phobos: { filter: 'blur', min: 0, max: 3, step: 0.1, unit: 'px', start: 3 },
+  heat: { filter: 'brightness', min: 1, max: 3, step: 0.1, unit: '', start: 3 },
+};
+
+export {
+  PICTURE_COUNT,
+  LIKE_MIN,
+  LIKE_MAX,
+  AVATAR_COUNT,
+  COMMENT_COUNT,
+  NAME,
+  MESSAGE,
+  DESCRIPTION,
+  COMMENTS_PER_PAGE,
+  REGEX_FOR_HASHTAG, HASHTAG_COUNT_MAX, HASHTAG_LENGTH_MAX, DESCRIPTION_LENGTH_MAX,
+  EFFECTS,
+};
